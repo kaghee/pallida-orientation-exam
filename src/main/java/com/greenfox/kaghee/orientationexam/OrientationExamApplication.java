866a4e7417a62c3d17847cb2d57b4df7a088515c
kaghee <kass.agnes@gmail.com>
2017-11-17 09:31:15 +0100

    initial commit, file structure created

---
+++ src/main/java/com/greenfox/kaghee/orientationexam/OrientationExamApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.kaghee.orientationexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientationExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientationExamApplication.class, args);
+	}
+}
